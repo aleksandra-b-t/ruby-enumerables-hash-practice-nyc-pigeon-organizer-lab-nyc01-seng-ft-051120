4b096239be3296adf08ca8662b9e5c1b12f47b96
ultimate-scope-5575 <a.bystranowska.takahashi@gmail.com>
2020-04-29 16:09:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,5 +7,7 @@ def nyc_pigeon_organizer(data)
           pigeon_list[name] = []
         end
        if pigeon_list[name][info] == nil
-         pigeon_list[name][info] =
+         pigeon_list[name][info] = []
+       end
+       
 end
