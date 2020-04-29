a0c639be84e58f567c10dba96274235b8339c315
ultimate-scope-5575 <a.bystranowska.takahashi@gmail.com>
2020-04-29 16:03:16 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,5 @@
 def nyc_pigeon_organizer(data)
-  new_keys << data[:gender][:male]
-  new_keys << data[:gender][:female]
+  pigeon_list = {}
+  pigeon_list << data[:gender][:male]
+  pigeon_list << data[:gender][:female]
 end
